8f919a484caea739bd49bb60df8e83a35e553fa3
Filip Vopalensky <fcoudy@seznam.cz>
2025-04-05 10:19:22 +0200

    test: Add project controller tests

--- annotator-be/src/test/java/cz/cuni/mff/vopalenf/annotator/api/controller/ProjectControllerTest.java
+++ annotator-be/src/test/java/cz/cuni/mff/vopalenf/annotator/api/controller/ProjectControllerTest.java
@@ -1,6 +1,10 @@
 package cz.cuni.mff.vopalenf.annotator.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import cz.cuni.mff.vopalenf.annotator.api.model.Project;
+import cz.cuni.mff.vopalenf.annotator.api.request.ProjectRequest;
+import cz.cuni.mff.vopalenf.annotator.exception.api.NotFoundException;
 import cz.cuni.mff.vopalenf.annotator.service.ProjectService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,14 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,8 +34,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProjectControllerTest {
 
+    private static final Long PROJECT_ID = 1L;
     private static final String PROJECT_NAME = "projectName";
     private static final String PROJECT_NAME_2 = "projectName2";
+    private static final String PRIORITY = "HIGH";
+    private static final LocalDate DUE_DATE = LocalDate.now();
+
+    private static final String NOT_FOUND_MESSAGE = "Project not found";
+    private static final String SCOPE = "project";
+
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     @MockBean
     ProjectService projectService;
@@ -37,7 +54,7 @@ class ProjectControllerTest {
     @Test
     @DisplayName("Should return all projects")
     @WithMockUser(authorities = {"ROLE_USER"})
-    void getTeam_ShouldReturnTeam_WhenAuthenticated() throws Exception {
+    void getAllProjects_ShouldReturnProjects_WhenAuthenticated() throws Exception {
         when(projectService.getAllProjects())
                 .thenReturn(List.of(
                         createProject(PROJECT_NAME),
@@ -50,9 +67,85 @@ class ProjectControllerTest {
                 .andExpect(jsonPath("$[*].projectName").value(containsInAnyOrder(PROJECT_NAME, PROJECT_NAME_2)));
     }
 
+    @Test
+    @DisplayName("Should return project by ID")
+    @WithMockUser(authorities = {"ROLE_USER"})
+    void getProjectById_ShouldReturnProject_WhenAuthenticated() throws Exception {
+        Project project = createProject(PROJECT_NAME);
+        when(projectService.getProject(PROJECT_ID)).thenReturn(project);
+
+        mockMvc.perform(get("/api/projects/{id}", PROJECT_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.projectName").value(PROJECT_NAME));
+    }
+
+    @Test
+    @DisplayName("Should return 404 when project not found by ID")
+    @WithMockUser(authorities = {"ROLE_USER"})
+    void getProjectById_ShouldReturn404_WhenProjectNotFound() throws Exception {
+        doThrow(new NotFoundException(NOT_FOUND_MESSAGE, SCOPE)).when(projectService).getProject(PROJECT_ID);
+
+        mockMvc.perform(get("/api/projects/{id}", PROJECT_ID))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Should delete project by ID")
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
+    void deleteProjectById_ShouldDeleteProject_WhenAuthenticated() throws Exception {
+        mockMvc.perform(delete("/api/projects/{id}", PROJECT_ID))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should return 404 when deleting project not found by ID")
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
+    void deleteProjectById_ShouldReturn404_WhenProjectNotFound() throws Exception {
+        doThrow(new NotFoundException(NOT_FOUND_MESSAGE, SCOPE)).when(projectService).deleteProject(PROJECT_ID);
+
+        mockMvc.perform(delete("/api/projects/{id}", PROJECT_ID))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Should update project by ID")
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
+    void updateProject_ShouldUpdateProject_WhenAuthenticated() throws Exception {
+        ProjectRequest projectRequest = createProjectRequest();
+        Project updatedProject = createProject(PROJECT_NAME);
+        when(projectService.updateProject(PROJECT_ID, projectRequest)).thenReturn(updatedProject);
+
+        mockMvc.perform(put("/api/projects/{projectId}", PROJECT_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(projectRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.projectName").value(PROJECT_NAME));
+    }
+
+    @Test
+    @DisplayName("Should return 404 when updating project not found by ID")
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
+    void updateProject_ShouldReturn404_WhenProjectNotFound() throws Exception {
+        ProjectRequest projectRequest = createProjectRequest();
+        when(projectService.updateProject(PROJECT_ID, projectRequest)).thenThrow(new NotFoundException(NOT_FOUND_MESSAGE, SCOPE));
+
+        mockMvc.perform(put("/api/projects/{projectId}", PROJECT_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(projectRequest)))
+                .andExpect(status().isNotFound());
+    }
+
     Project createProject(String name) {
         return Project.builder()
                 .projectName(name)
+                .build();
+    }
+
+    ProjectRequest createProjectRequest() {
+        return ProjectRequest.builder()
+                .projectName(PROJECT_NAME)
+                .deadline(DUE_DATE)
+                .priority(PRIORITY)
                 .build();
     }
 }
