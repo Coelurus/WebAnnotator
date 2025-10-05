1f0e6e67cd3e4a1e661fe76997e7572cb994efdd
Filip Vopalensky <fcoudy@seznam.cz>
2025-10-05 22:58:07 +0200

    Migrate to Postgre from H2

--- annotator-be/src/main/resources/db/migration/common/V2_0_0__create_admin_users.sql
+++ annotator-be/src/main/resources/db/migration/common/V2_0_0__create_admin_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (id, first_name, last_name, username, password_hash, team_id, role)
 VALUES (0, 'Admin', 'Admin', 'admin', '$2a$10$btgrXn5reBv5nblarOU3leWnfZuyJAuKFwhliXL8S9S5yspfdIzka', null, 'ROLE_ADMIN');
-ALTER TABLE users ALTER COLUMN id RESTART WITH (SELECT MAX(id) + 1 FROM users);
+SELECT setval('users_id_seq', (SELECT MAX(id) + 1 FROM users), false);
